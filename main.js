0e14daf1faf833b221899da8f0a145aef67025e3
Tan Chee Seong <tancheeseong1975@gmail.com>
2017-02-08 14:07:38 +0800

    day 3 after lunch

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-var express = require("express");
+var express = require("express"); // express is the webserver
 var path    = require("path");
 var filesys = require("fs");
 
-// create an instance of express app
+// create an instance of express app - webserver
 var app = express();
 
-app.use(express.static(path.join(__dirname + "/public")));
-app.use("/images", express.static(path.join(__dirname + "/images")));
-
-app.use("/picture", function(req, resp){
-    // hardcode filename
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images" , express.static(path.join(__dirname, "images")));
+app.use("/picture", function(req, res){
+    //hardcode filename
     //var imagefilnameArr = ["pic01.jpg", "pic02.jpg", "pic03.jpg", "pic04.jpg", "pic05.jpg", "pic06.jpg"];
     
     //lists down the content of the directory and make it into array
     var imagefilnameArr = filesys.readdirSync('./images/');
-
-    var randImgIdx = Math.floor(Math.random() * imagefilnameArr.length);
-
-    var imgfilename = imagefilnameArr[randImgIdx];
+    var randImgIdx      = Math.floor(Math.random() * imagefilnameArr.length);
+    var imgfilename     = imagefilnameArr[randImgIdx];
 
     //resp.type("text/html"); //Representation of the resource
     //resp.send("<img src='/images/" + imgfilename + "'>");
 
-    resp.type("image/jpeg");
-    resp.sendfile(__dirname + "/images/" + imgfilename);
+    res.type("image/jpeg");
+    res.sendFile(__dirname + "/images/" + imgfilename);
 });
 
-
-
+app.use(function(req, res) {
+    res.status(440);
+    res.send("error file not found");
+});
 
 /*
 // process is a speacial node predefine variable 
@@ -35,11 +34,10 @@ for(var i=0; i<process.argv.length; i++) {
     console.info("argv[%d] : %s, type >>> %s", i, process.argv[i], typeof process.argv[i]);
 }
 */
-
 //console.info(">>> app port: %s", process.env.APP_PORT);
-
 app.set("port",  parseInt(process.argv[2]) ||  parseInt(process.env.APP_PORT) || 3000);  
 app.listen(app.get("port"), function() {
-    console.info("Application is listening to port "+ app.get("port"));
+    console.info("Application started at %s is listening to port %d", new Date(), app.get("port"));
 });
+
 
